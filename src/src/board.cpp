8cde516c59b39b8298f595d612252b502716ca4e
duckyfied <rounakpaul007@gmail.com>
2025-02-14 02:14:08 +0530

    Refactor code

--- src/src/board.cpp
+++
@@ -1,8 +0,0 @@
-#include "../headers/board.h"
-#include <iostream>
-#include <random>
-
-Board::Board() : board(64, Piece::None), sideToMove(Piece::White),
-              canCastleKingsideWhite(false), canCastleQueensideWhite(false),
-              canCastleKingsideBlack(false), canCastleQueensideBlack(false),
-              enPassantTarget(-1) {}
